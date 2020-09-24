d9d1b39523563adfbbb47f24fc3efb39e0a19460
Dashi Cao <dscao999@gmail.com>
2020-09-24 21:09:13 +0800

    Install openssh-server

--- check-cdrom.sh
+++ check-cdrom.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
+if [ -n "$1" ]
+then
+	CDDIR=$1
+else
+	CDDIR=/media/cdrom
+fi
+#
 dpkg --list | awk '/^ii/ {print $2}' | \
 while read pkg_name
 do
-	pkg = ${pkg_name%%:*}
-	fname=$(find /media/cdrom -name ${pkg}\* -print 2> /dev/null)
+	pkg=${pkg_name%%:*}
+	fname=$(find $CDDIR -name ${pkg}\* -print 2> /dev/null)
 	[ -z "$fname" ] || continue
 	if ! apt-cache show $pkg > /dev/null 2>&1
 	then
