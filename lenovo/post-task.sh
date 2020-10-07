d964926045d13ee5732d8caac60717ff536afb32
Dashi Cao <dscao999@gmail.com>
2020-10-07 10:02:34 +0800

    Set time server to cn.pool.ntp.org

--- lenovo/post-task.sh
+++ lenovo/post-task.sh
@@ -71,6 +71,63 @@ if [ x"$FROM_UPSTART" = x"yes" ]; then
 fi
 ENDDOC
 #
+# add auto-login for ctos
+#
+cat >> /etc/init/autologin-ctos.conf <<"ENDDOC1"
+#
+# Autologin - Automatically login as ctos on tty7
+#
+# based on nodm
+
+description	"X starter"
+author		"Wang Shishuang <wangss@cloud-times.com>"
+
+start on ((filesystem and stopped rc
+           and runlevel [!06]
+           and started dbus
+           and (drm-device-added card0 PRIMARY_DEVICE_FOR_DISPLAY=1
+                or stopped udev-fallback-graphics))
+          or runlevel PREVLEVEL=S)
+
+stop on runlevel [016]
+
+respawn
+
+emits login-session-start
+emits desktop-session-start
+emits desktop-shutdown
+
+script
+    if [ -n "$UPSTART_EVENTS" ]
+    then
+        # Check kernel command-line for inhibitors, unless we are being called
+        # manually
+        for ARG in $(cat /proc/cmdline); do
+            if [ "$ARG" = "text" ]; then
+		plymouth quit || : 
+                stop
+		exit 0
+            fi
+        done
+
+	if [ "$RUNLEVEL" = S -o "$RUNLEVEL" = 1 ]
+	then
+	    # Single-user mode
+	    plymouth quit || :
+	    exit 0
+	fi
+    fi
+
+    exec /sbin/getty -8 38400 -o "-f \u FROM_UPSTART=yes" -a ctos tty7
+end script
+
+post-stop script
+	if [ "$UPSTART_STOP_EVENTS" = runlevel ]; then
+		initctl emit desktop-shutdown
+	fi
+end script
+ENDDOC1
+#
 rm -f /etc/rc.local
 mv /etc/rc.local.orig /etc/rc.local
 sleep 5
