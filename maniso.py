e64f5a99ddfcb8a929bd1fcb17722c0bc643951b
Dashi Cao <dscao999@gmail.com>
2020-10-10 10:50:45 +0800

    Add an ISO manipulation tool

---
+++ maniso.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+#
+import os, sys, stat
+import shutil
+import tkinter as tk
+
+mfont = ('courier', 16, 'bold')
+
+def pre_check(isodir):
+    isodir = isodir.rstrip('/')
+    if not os.path.ismount(isodir):
+        print("Error: {} is not a mount point.".format(isodir))
+        sys.exit(1)
+    info = isodir + '/.disk/info'
+    if not os.path.isfile(info):
+        print("Error: {} is not a LIOS ISO image.".format(isodir))
+        sys.exit(2)
+    with open(info, 'r') as inf:
+        label = inf.read()
+        if label.find('Lenovo LIOS') != 0:
+            print("Error: {} is not an legitimate LIOS image.".format(isodir))
+            sys.exit(3)
+
+def remove_rdonly(func, path, _):
+    st = os.stat(path, follow_symlinks=False)
+    if (st.st_mode & stat.S_IWRITE) == 0:
+        os.chmod(path, stat.S_IWRITE|st.st_mode)
+    dr = os.path.dirname(path)
+    st = os.stat(dr, follow_symlinks=False)
+    if (st.st_mode & stat.S_IWRITE) == 0:
+        os.chmod(dr, st.st_mode|stat.S_IWRITE)
+    func(path)
+
+def parse_preseed(rootdev, clock_setup, time_svr):
+    seed = topdir + '/preseed/ubuntu-server.seed'
+    inf = open(seed, 'r')
+    line = inf.readline()
+    while len(line) > 0:
+        lns = line.split()
+        if len(lns) < 4:
+            line = inf.readline()
+            continue
+
+        if lns[0] == "partman-auto" and lns[1] == "partman-auto/disk" and lns[2] == "string":
+            rootdev = lns[3]
+        elif lns[0] == "d-i" and lns[1] == "clock-setup/ntp-server" and lns[2] == "string":
+            time_svr = lns[3]
+        elif lns[0] == "d-i" and lns[1] == "clock-setup/ntp" and lns[2] == "boolean":
+            if lns[3] == "true":
+                clock_setup = True
+
+        line = inf.readline()
+    inf.close()
+
+if len(sys.argv) < 2:
+    print("Error: An LIOS ISO mount point must be specified.")
+    sys.exit(5)
+
+isodir = sys.argv[1]
+topdir = "isotop-"+str(os.getpid())
+
+pre_check(isodir)
+
+#shutil.copytree(isodir, topdir, symlinks=True)
+
+rootdev = ''
+time_svr = ''
+clock_setup = False
+
+#parse_preseed(rootdev, clock_setup, time_svr)
+
+#if len(rootdev) == 0:
+#    print("No Root Device, Invalid LIOS ISO.")
+#    print("Deleting working space. Please wait for a while...")
+#    shutil.rmtree(topdir, onerror=remove_rdonly)
+#    sys.exit(6)
+
+root = tk.Tk()
+#root.title(sys.argv[0])
+root.wm_title(sys.argv[0])
+
+uf = tk.Frame(root)
+uf.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
+u_uf = tk.Frame(uf)
+u_uf.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
+rdev_label = tk.Label(u_uf, text="root device:", font=mfont)
+rdev_label.pack(side=tk.LEFT)
+rdev_text = tk.Entry(u_uf, font=mfont)
+rdev_text.insert(0, rootdev)
+rdev_text.pack(side=tk.RIGHT)
+l_uf = tk.Frame(uf)
+l_uf.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
+border = tk.Frame(l_uf, height=2, bd=1, bg='red', relief=tk.SUNKEN)
+border.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
+
+lf = tk.Frame(root)
+lf.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
+
+u_lf = tk.Frame(lf)
+set_clock = tk.IntVar()
+if clock_setup == True:
+    set_clock.set(1)
+else:
+    set_clock.set(0)
+chk_clock = tk.Checkbutton(u_lf, text="Set Clock", variable=set_clock, font=mfont)
+chk_clock.pack(side=tk.TOP)
+u_lf.pack(side=tk.TOP, expand=tk.YES, fill=tk.X)
+
+l_lf = tk.Frame(lf)
+ntp_label = tk.Label(l_lf, text="NTP Server:", font=mfont)
+ntp_label.pack(side=tk.LEFT)
+ntp_ip = tk.Entry(l_lf, font=mfont)
+if set_clock.get() == 1:
+    ntp_ip.insert(0, time_svr)
+else:
+    ntp_ip.config(state='disabled')
+ntp_ip.pack(side=tk.RIGHT)
+l_lf.pack(side=tk.BOTTOM, expand=tk.YES, fill=tk.X)
+
+root.mainloop()
+
+print("Deleting working space. Please wait for a while...")
+#shutil.rmtree(topdir, onerror=remove_rdonly)
+sys.exit(0)
