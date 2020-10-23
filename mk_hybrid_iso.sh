da28f66813ae783f9f0fe8d4798ebc8ee7378f40
Dashi Cao <dscao999@gmail.com>
2020-10-23 16:43:52 +0800

    Complete Debian Preseeding with Xfce installed

--- mk_hybrid_iso.sh
+++ mk_hybrid_iso.sh
@@ -1,4 +1,13 @@
-ISODIR=isotop
+#!/bin/bash
+#
+ISODIR=$1
+[ -z "$ISODIR" ] && ISODIR=isotop
+if [ ! -d "$ISODIR" ]
+then
+	echo "Invalid ISO Top Directory: $ISODIR"
+	exit 1
+fi
+#
 cp /usr/lib/ISOLINUX/isolinux.bin $ISODIR/isolinux/isolinux.bin
 xorriso -as mkisofs -r -V 'LENOVO_LIOS_V1_AMD64' \
 	-o hybrid_dvd.iso \
