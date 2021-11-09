426973f2f277df72a2efd8713982a1a5171b205c
Dashi Cao <dscao999@gmail.com>
2021-11-09 20:38:30 +0800

    Use variable arguments macro for error output

--- utils/udev_events.c
+++ utils/udev_events.c
@@ -8,9 +8,7 @@
 #include <poll.h>
 #include <errno.h>
 #include <time.h>
-#ifdef NDEBUG
 #include <syslog.h>
-#endif
 #include <stdarg.h>
 #include <getopt.h>
 #include <signal.h>
@@ -29,26 +27,8 @@ static void sighand(int sig)
 		child_exit = 1;
 }
 
-#ifdef NDEBUG
-static inline void logerr(const char *fmt, ...)
-{
-	va_list ap;
-
-	va_start(ap, fmt);
-	vsyslog(LOG_ERR, fmt, ap);
-	va_end(ap);
-}
-#else
-static inline void logerr(const char *fmt, ...)
-{
-	va_list ap;
-
-	va_start(ap, fmt);
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, "\n");
-	va_end(ap);
-}
-#endif
+#define logerr(fmt, ...) syslog(LOG_ERR, fmt, __VA_ARGS__)
+#define prterr(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__)
 
 static int mkpipe(const char *pipe)
 {
@@ -60,28 +40,28 @@ static int mkpipe(const char *pipe)
 	sysret = stat(pipe, &st);
 	if (sysret == 0) {
 		if ((st.st_mode & S_IFMT) != S_IFIFO ) {
-			fprintf(stderr, "'%s' is not a fifo", pipe);
+			prterr("'%s' is not a fifo", pipe);
 			goto exit_10;
 		}
 		uid = getuid();
 		gid = getgid();	
 		if ((uid != 0 || gid != 0) &&
 				(st.st_uid != uid || st.st_gid != gid)) {
-			fprintf(stderr, "'%s' is not ownd by you", pipe);
+			prterr("'%s' is not ownd by you", pipe);
 			goto exit_10;
 		}
 		if ((st.st_mode & (~S_IFMT)) != 0600) {
-			fprintf(stderr, "'%s' mode is not 0600", pipe);
+			prterr("'%s' mode is not 0600", pipe);
 			goto exit_10;
 		}
 	} else {
 		if (errno != ENOENT) {
-			fprintf(stderr, "Cannot stat '%s': %s", pipe, strerror(errno));
+			prterr("Cannot stat '%s': %s", pipe, strerror(errno));
 			goto exit_10;
 		}
 		sysret = mknod(pipe, 0600|S_IFIFO, 0);
 		if (sysret == -1) {
-			fprintf(stderr, "Cannot mkpipe '%s': %s", pipe, strerror(errno));
+			prterr("Cannot mkpipe '%s': %s", pipe, strerror(errno));
 			goto exit_10;
 		}
 	}
@@ -101,14 +81,10 @@ static void send_info(const char *pipe)
 	keyboard = getenv("ID_INPUT_KEYBOARD");
 	if (keyboard == NULL || keyboard[0] != '1')
 		return;
-#ifdef NDEBUG
 	openlog(NULL, 0, 0);
-#endif
 	devpath = getenv("DEVPATH");
-	if (devpath == NULL) {
-		logerr("ID_INPUT_KEYBOARD without DEVPATH");
+	if (devpath == NULL)
 		return;
-	}
 
 	sysret = stat(pipe, &pst);
 	if (sysret == -1) {
@@ -129,9 +105,7 @@ static void send_info(const char *pipe)
 		logerr("Send %d bytes to %s\n", sysret, pipe);
 
 	close(fd);
-#ifdef NDEBUG
 	closelog();
-#endif
 }
 
 static void xset_keyboard(int delay)
@@ -141,7 +115,7 @@ static void xset_keyboard(int delay)
 
 	child = fork();
 	if (child == -1)
-		fprintf(stderr, "Cannot fork: %s\n", strerror(errno));
+		prterr("Cannot fork: %s\n", strerror(errno));
 	else if (child == 0) {
 		struct timespec itv;
 		itv.tv_sec = 0;
@@ -149,14 +123,14 @@ static void xset_keyboard(int delay)
 		nanosleep(&itv, NULL);
 		if (verbose == 1) {
 			clock_gettime(CLOCK_MONOTONIC_COARSE, &itv);
-			fprintf(stderr, "[%6ld.%06ld] execute setxkbmap " \
-					"-option srvrkeys:none\n",
+			prterr("[%6ld.%06ld] execute setxkbmap -option " \
+					"srvrkeys:none\n",
 					itv.tv_sec, (itv.tv_nsec)/1000);
 		}
 		sysret = execl("/usr/bin/setxkbmap", "setxkbmap", "-option",
 				"srvrkeys:none", NULL);
 		if (sysret == -1)
-			fprintf(stderr, "execl failed: %s\n", strerror(errno));
+			prterr("execl failed: %s\n", strerror(errno));
 		exit(1);
 	}
 }
@@ -173,7 +147,7 @@ static int recv_info(const char *pipe, int delay)
 		do {
 			fd = open(pipe, O_RDONLY);
 			if (fd == -1 && errno != EINTR) {
-				fprintf(stderr, "Cannot open '%s' for " \
+				prterr("Cannot open '%s' for " \
 						"reading: %s\n", pipe,
 						strerror(errno));
 				return 2;
@@ -190,7 +164,7 @@ static int recv_info(const char *pipe, int delay)
 			pfd.revents = 0;
 			sysret = poll(&pfd, 1, 500);
 			if (sysret == -1 && errno != EINTR) {
-				fprintf(stderr, "poll on '%s' failed: %s\n",
+				prterr("poll on '%s' failed: %s\n",
 						pipe, strerror(errno));
 				break;
 			} else if (sysret == 0)
@@ -201,12 +175,12 @@ static int recv_info(const char *pipe, int delay)
 				continue;
 			sysret = read(fd, buf, sizeof(buf));
 			if (sysret == -1) {
-				fprintf(stderr, "Read from '%s' failed: %s\n",
+				prterr("Read from '%s' failed: %s\n",
 						pipe, strerror(errno));
 				retv = 4;
 				break;
 			} else if (sysret == 0) {
-				fprintf(stderr, "Read 0 bytes from pipe '%s'\n",
+				prterr("Read 0 bytes from pipe '%s'\n",
 						pipe);
 				continue;
 			}
@@ -261,12 +235,12 @@ int main(int argc, char *argv[])
 			delay = atoi(optarg);
 			if (delay < 0) {
 				delay = 150;
-				fprintf(stderr, "Invalid delay %d, set to " \
+				prterr("Invalid delay %d, set to " \
 						"150 milliseconds\n", delay);
 			}
 			if (delay > 1000) {
 				delay = delay % 1000;
-				fprintf(stderr, "More than one second, " \
+				prterr("More than one second, " \
 						"truncated to %d\n", delay);
 			}
 			break;
@@ -274,10 +248,10 @@ int main(int argc, char *argv[])
 			verbose = 1;
 			break;
 		case '?':
-			fprintf(stderr, "Unknown option: %c\n", optopt);
+			prterr("Unknown option: %c\n", optopt);
 			break;
 		case ':':
-			fprintf(stderr, "Missing argument for %c\n", optopt);
+			prterr("Missing argument for %c\n", optopt);
 			break;
 		default:
 			assert(0);
@@ -297,14 +271,14 @@ int main(int argc, char *argv[])
 		sact.sa_mask = mset;
 		sact.sa_flags = 0;
 		if (sigaction(SIGINT, &sact, NULL) == -1)
-			fprintf(stderr, "signal install failed for SIGINT: " \
+			prterr("signal install failed for SIGINT: " \
 					"%s\n", strerror(errno));
 		if (sigaction(SIGTERM, &sact, NULL) == -1)
-			fprintf(stderr, "signal install failed for SIGTERM: " \
+			prterr("signal install failed for SIGTERM: " \
 					"%s\n", strerror(errno));
 		sact.sa_flags = SA_NOCLDWAIT;
 		if (sigaction(SIGCHLD, &sact, NULL) == -1)
-			fprintf(stderr, "signal install failed for SIGCHLD: " \
+			prterr("signal install failed for SIGCHLD: " \
 					"%s\n", strerror(errno));
 		retv = recv_info(pipe, delay);
 		unlink(pipe);
